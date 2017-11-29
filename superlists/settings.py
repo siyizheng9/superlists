5e9b3ae8a1a73aa6f9b1a56abfa3ff521f72d8d2
siyi.zheng <siyi.zheng@aalto.fi>
2017-11-29 16:39:00 +0200

    add email settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -143,3 +143,9 @@ LOGGING = {
     },
     'root': {'level': 'INFO'},
 }
+
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST_USER = 'superlists.me@outlook.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
