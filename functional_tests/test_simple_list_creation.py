279f9c56394fddb51af5616b186c1f6840cf8dfe
siyi.zheng <siyi.zheng@aalto.fi>
2017-11-30 13:27:17 +0200

    refactoring

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,6 +1,5 @@
 from .base import FunctionalTest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(FunctionalTest):
@@ -24,18 +23,14 @@ class NewVisitorTest(FunctionalTest):
 
         # She types "buy peacock feathers" into a text box (Edith's hobby
         # # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
+        self.add_list_item('Buy peacock feathers')
 
         # When she hits ener, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        inputbox = self.get_item_input_box()
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
+        self.add_list_item('Use peacock feathers to make a fly')
 
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
@@ -50,10 +45,7 @@ class NewVisitorTest(FunctionalTest):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
-        inputbox = self.get_item_input_box()
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.add_list_item('Buy peacock feathers')
 
         # She notices that her list has a unique URL
         edith_list_url = self.browser.current_url
@@ -75,10 +67,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith...
-        inputbox = self.get_item_input_box()
-        inputbox.send_keys('Buy milk')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy milk')
+        self.add_list_item('Buy milk')
 
         # Francis get his own unique URL
         francis_list_url = self.browser.current_url
