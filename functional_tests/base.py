6f164ef2dc7518eb708471d6bad9b9001830c39f
siyi.zheng <siyi.zheng@aalto.fi>
2017-11-30 13:20:36 +0200

    add add_list_item helper function & test case for my_lists

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
 from .server_tools import reset_database
 import time
 import os
@@ -21,6 +22,9 @@ def wait(fn):
     return modified_fn
 
 
+
+
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -32,6 +36,14 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def add_list_item(self, item_text):
+        num_rows = len(self.browser.find_elements_by_css_selector(
+            '#id_list_table tr'))
+        self.get_item_input_box().send_keys(item_text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        item_number = num_rows + 1
+        self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
 
     @wait
     def wait_for_row_in_list_table(self, row_text):
