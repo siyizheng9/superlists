82ef9b92af5ed592efd2a66b2f0c5e7aac2b0a93
siyi.zheng <siyi.zheng@aalto.fi>
2017-11-30 11:40:10 +0200

    create pre authentication session on remote server

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from .server_tools import reset_database
 import time
 import os
 
@@ -27,6 +28,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
