80782e22865e68ac8a2fe827f918502e5c15d871
siyi.zheng <siyi.zheng@aalto.fi>
2017-12-01 16:37:06 +0200

    check sequence of executations

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,6 +6,7 @@ from lists.forms import (
     DUPLICATE_ITEM_ERROR, ExistingListItemForm
 )
 from django.contrib.auth import get_user_model
+from unittest.mock import patch
 User = get_user_model()
 
 
@@ -123,12 +124,22 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'text': ''})
         self.assertIsInstance(response.context['form'], ItemForm)
 
-    def test_list_owner_is_saved_if_user_is_authenticated(self):
+    @patch('lists.views.List')
+    @patch('lists.views.ItemForm')
+    def test_list_owner_is_saved_if_user_is_authenticated(
+        self, mockItemFormClass, mockListClass
+    ):
         user = User.objects.create(email='a@b.com')
         self.client.force_login(user)
+        mock_list = mockListClass.return_value
+
+        def check_owner_assigned():
+            self.assertEqual(mock_list.owner, user)
+        mock_list.save.side_effect = check_owner_assigned
+
         self.client.post('/lists/new', data={'text': 'new item'})
-        list_ = List.objects.first()
-        self.assertEqual(list_.owner, user)
+
+        mock_list.save.assert_called_once_with()
 
 
 class NewItemTest(TestCase):
